50100156184a56d9b1e516bcc44924ae2a8134ad
Sergey Loskutnikov <nitche9@gmail.com>
2024-01-31 19:11:43 +0300

    little icon remove

--- resource.h
+++ resource.h
@@ -3,8 +3,6 @@
 // Used by BordlessWindows.rc
 //
 #define IDI_ICON1                       101
-#define IDB_BITMAP1                     105
-#define IDI_ICON2                       106
 #define IDI_ICON3                       107
 
 // Next default values for new objects
